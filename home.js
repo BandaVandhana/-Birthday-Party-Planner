775f4ad1d77f4d886f7688997cb4a1d8eacd9cbb
BandaVandhana <153309442+BandaVandhana@users.noreply.github.com>
2024-05-18 18:34:36 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,36 @@
+ <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView:4,
+      spaceBetween: 30,
+      loop: true,
+      speed: 1000,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+       breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        480: {
+          slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+	
+    
+   
+    
+    
+   
+  </script>
